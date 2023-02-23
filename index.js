9daa705332fbb3b8ebaaebd4dd730c70dd63ed42
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2023-02-23 09:38:05 +0330

    Adding Hamburger Menu

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const btn = document.getElementById("btn");
 const light = document.querySelector(".light");
+const menuImg = document.querySelector(".menu-img");
+const menu = document.querySelector(".menu");
 
 btn.addEventListener("click", () => {
     btn.classList.toggle("active");
     light.classList.toggle("on")
+})
+
+menuImg.addEventListener("click", () => {
+    menu.classList.toggle("menu-active");
 })
